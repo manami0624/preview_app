41d5fcba502d272f4d2a40296fe6d37eee94fa16
manami0624 <manana624@yahoo.co.jp>
2023-07-25 19:05:09 +0900

    投稿枚数に制限をかける実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   has_many_attached :images
   validates :text, presence: true
   validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は１枚以上５枚以下にしてください"}
 end
